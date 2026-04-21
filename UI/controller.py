68fac10ac15f24de38fcca4f3d651bb43be5627a
s326209 <s326209@studenti.polito.it>
2026-04-21 14:58:23 +0200

    completa il primo punto

--- UI/controller.py
+++ UI/controller.py
@@ -12,15 +12,63 @@ class Controller:
         self._model = model
         # other attributes
         self._mese = 0
+        self._allSituazioni = None
+        self._giornateTrascorseCitta = {"Genova":0, "Torino":0, "Milano":0}
+        self._giornateConsecutive = 0
+
+
 
     def handle_umidita_media(self, e):
-        pass
+        self._view.lst_result.clean()
+        self._view.update_page()
+        if self._mese==0:
+            self._view.create_alert("Scegliere un mese!")
+            return
+        if not self._allSituazioni:
+            self._allSituazioni = self._model.getAllSituazioni()
+        sommeUmidita = {"Genova":0, "Torino":0, "Milano":0}
+        contatoriApparazioni = {"Genova":0, "Torino":0, "Milano":0}
+        for s in self._allSituazioni:
+            if (self._mese == s.data.month):
+                sommeUmidita[s.localita] += int(s.umidita)
+                contatoriApparazioni[s.localita] += 1
 
+        medieUmidita = {"Genova": sommeUmidita["Genova"]/contatoriApparazioni["Genova"], "Torino": sommeUmidita["Torino"]/contatoriApparazioni["Torino"], "Milano": sommeUmidita["Milano"]/contatoriApparazioni["Milano"]}
+        #medieUmiditaSorted = dict(sorted(medieUmidita.items(), key=lambda item: item[1], reverse=True))
+        self._view.lst_result.controls.append(ft.Text(f"L'umidità media nel mese selezionato è:"))
+        for citta,media in medieUmidita.items():
+            self._view.lst_result.controls.append(ft.Text(f"{citta}: {round(media,4)}  (giorni contati: {contatoriApparazioni[citta]})"))
+        self._view.update_page()
 
+        return
 
     def handle_sequenza(self, e):
-        pass
+        if self._mese==0:
+            self._view.create_alert("Scegliere un mese!")
+            return
+        if not self._allSituazioni:
+            self._allSituazioni = self._model.getAllSituazioni()
+        situazioniMese = [s for s in self._allSituazioni if (self._mese==s.data.month and s.data.day<=15)]
+        situazioniMese.sort(key=lambda s:s.data)
+
+        for i in range(1,16):
+            situazioniGiorno = []
+            for s in situazioniMese:
+
+                if s.data.day == i:
+                    situazioniGiorno.append(s)
+                if len(situazioniGiorno) == 3:
+                    break
+            umiditaMinimaGiorno = min([s.umidita for s in situazioniGiorno])
+
+
+
+
+        self.recursion(situazioniMese)
+        return
 
     def read_mese(self, e):
         self._mese = int(e.control.value)
 
+    def recursion(self,situazioni):
+        pass
--- model/model.py
+++ model/model.py
@@ -1,3 +1,8 @@
+from database.meteo_dao import MeteoDao
+
+
 class Model:
     def __init__(self):
         pass
+    def getAllSituazioni(self):
+        return MeteoDao.get_all_situazioni()
